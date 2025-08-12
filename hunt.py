bcbabebac46ca3a68251dbfebdb2739563bb6144
nnavid65 <nnavid65@outlook.com>
2025-08-12 13:02:18 +0200

    creat the classes

---
+++ hunt.py
@@ -0,0 +1,8 @@
+class Hunter():
+    def __init__(self, name, breed, move_speed, hunting_ability):
+
+        self.name = name
+        self.breed = breed
+        self.move_speed = move_speed
+        self.hunting_ability = hunting_ability
+
---
+++ hunter.py
@@ -0,0 +1,6 @@
+class Hunter():
+    def __init__(self, name, breed, move_speed, hunted):
+        self.name = name
+        self.breed = breed
+        self.move_speed = move_speed
+        self.hunted = hunted
